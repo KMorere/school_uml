43d06ca817eeb011e1d28dcca1e64a8faa723c27
Antony Vitry <antony.vitry@fms-ea.com>
2025-11-19 22:50:24 +0100

    Update Java: Date - class finish with docstring

--- Java/src/schooluml/Date.java
+++ Java/src/schooluml/Date.java
@@ -4,11 +4,97 @@ package schooluml;
  * Class representing a Date
  */
 public class Date {
-    /**
-     * Constructor
-     */
-    public Date() {
+    // Atributs
+    private int id;
+    private int numberDay;
+    private String month;
+    private int year;
 
+    private static int countId = 0;
+
+    // Constructeurs
+    public Date(int id, int numberDay, String month, int year){
+        /**
+         * @param id
+         * @param numberDay
+         * @param month
+         * @param year
+         */
+        setId();
+        setNumberDay(numberDay);
+        setMonth(month);
+        setYear(year);
     }
 
+    // Accesseurs
+    public int getCountId() {
+        /**
+         * @return countId
+         */
+        return countId;
+    }
+
+    public void setCountId() {
+        /**
+         * @param countId
+         */
+        countId = getCountId() + 1;
+    }
+
+    public int getId() {
+        /**
+         * @return id
+         */
+        return this.id;
+    }
+
+    public void setId() {
+        /**
+         * @param id
+         */
+        setCountId();
+        this.id = getCountId();
+    }
+
+    public int getNumberDay() {
+        /**
+         * @return numberDay
+         */
+        return this.numberDay;
+    }
+
+    public void setNumberDay(int numberDay) {
+        /**
+         * @param numberDay
+         */
+        this.numberDay = numberDay;
+    }
+
+    public String getMonth() {
+        /**
+         * @return month
+         */
+        return this.month;
+    }
+
+    public void setMonth(String month) {
+        /**
+         * @param month
+         */
+        this.month = month;
+    }
+
+    public int getYear() {
+        /**
+         * @return year
+         */
+        return this.year;
+    }
+
+    public void setYear(int year) {
+        /**
+         * @param year
+         */
+        this.year = year;
+    }
 }
