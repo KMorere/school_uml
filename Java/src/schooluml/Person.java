d38ed529892aad2a926e7bdd6a5bfefac3764f2e
Daphne Cluseau <daphne.cluseau@fms-ea.com>
2025-11-19 14:10:03 +0100

    Adding Person, Student, Teacher and Admin classes, with constructors, getters and setters

--- Java/src/schooluml/Person.java
+++ Java/src/schooluml/Person.java
@@ -4,11 +4,117 @@ package schooluml;
  * Class representing a person
  */
 public abstract class Person {
-/**
- * Constructor
- */
-	public Person() {
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String address;
+	private Planning planning;
+
+	/**
+	 * Constructor
+	 */
+	Person() {
 
 	}
+	
+	/**
+	 * Constructor with all parameters
+	 * @param firstname
+	 * @param lastName
+	 * @param age
+	 * @param planning
+	 */
+	Person(String firstname, String lastName, int age, Planning planning) {
+		this.firstName = firstname;
+		this.lastName = lastName;
+		this.age = age;
+		this.planning = planning;
+	}
+	
+	/**
+	 * Constructor with all parameters minus planning
+	 * @param firstname
+	 * @param lastName
+	 * @param age
+	 */
+	Person(String firstname, String lastName, int age) {
+		this.firstName = firstname;
+		this.lastName = lastName;
+		this.age = age;
+		this.planning = new Planning();
+	}
 
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	/**
+	 * @return the address
+	 */
+	public Planning getPlanning() {
+		return this.planning;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setPlanning(Planning planning) {
+		this.planning = planning;
+	}
+
+	public void participate() {
+
+	}
 }
