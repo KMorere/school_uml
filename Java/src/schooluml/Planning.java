2c591d7b0242da51bccaa7f54fbd9ba53e9c18a6
Antony Vitry <antony.vitry@fms-ea.com>
2025-11-19 15:21:42 +0100

    Update Java: Planning - class finish with docstring

--- Java/src/schooluml/Planning.java
+++ Java/src/schooluml/Planning.java
@@ -4,11 +4,49 @@ package schooluml;
  * Class representing a Planning
  */
 public class Planning {
-    /**
-     * Constructor
-     */
-    public Planning() {
+    // Atributs
+    private int id;
+    private TreeMap<Date, Subject> planning;
 
+    // Constructeurs
+    public Planning(int id, Date date, Subject subject) {
+        /**
+         * @param id
+         * @param date
+         * @param subject
+         */
+        setId(id);
+        setPlanning(date, subject);
     }
 
+    // Accesseurs
+    public int getId() {
+        /**
+         * @return id
+         */
+        return this.id;
+    }
+
+    public void setId(int id) {
+        /**
+         * @param id
+         */
+        this.id = id;
+    }
+
+    public TreeMap<Date, Subject> getPlanning() {
+        /**
+         * @return planning
+         */
+        return this.planning;
+    }
+
+    public void setPlanning(Date date, Subject subject) {
+        /**
+         * @param date
+         * @param subject
+         */
+        this.planning = new TreeMap<>();
+        this.planning.put(date, subject)
+    }
 }
