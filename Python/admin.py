4649511e226e0ded71e671a3d4503aa394309a40
matthieu-27 <matthieu27@pm.me>
2025-11-20 15:24:33 +0100

    feat: Created Admin class with teach and participate

---
+++ Python/admin.py
@@ -0,0 +1,13 @@
+from person import Person
+
+
+class Admin(Person):
+    """ A class that represents the school director """
+    def __init__(self, _first_name: str, _last_name: str, _age: int):
+        super().__init__(_first_name, _last_name, _age, None, None)
+
+    def teach(self):
+        super()
+
+    def participate(self):
+        super()
