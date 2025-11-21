240869c2705ac0d7127cc2fc33c7b2b63861fded
m.couderc <m.couderc@komet-is.fr>
2025-11-21 09:27:59 +0100

    feat: added set_adress and set_planning methods to Person

--- Python/admin.py
+++ Python/admin.py
@@ -3,6 +3,7 @@ from person import Person
 
 class Admin(Person):
     """ A class that represents the school director """
+
     def __init__(self, _first_name: str, _last_name: str, _age: int):
         super().__init__(_first_name, _last_name, _age, None, None)
 
