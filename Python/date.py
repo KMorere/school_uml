c3ca46fa9f6453625dcaa42b8dfed504efc0670c
Toky Sigogneau <toky.sigogneau@fms-ea.com>
2025-11-21 09:51:42 +0100

    added some comments on planning files

--- Python/date.py
+++ Python/date.py
@@ -1,11 +1,12 @@
 class Date :
 
+    #constructor
     def __init__(self,day:int,month:int, year:int):
         self.day:int = day
         self.month:int = month
         self.year:int = year
 
-
+    #display
     def __str__(self):
         return f"{self.day}/{self.month}/{self.year}"
 
--- Python/test_planning.py
+++ Python/test_planning.py
@@ -4,10 +4,10 @@ from planning import Planning
 
 toto = Date(19,11,2025)
 
-papaa = Planning(Date(10,6,2016), ([Subject("Math"), Subject("Anglais")]))
+papa = Planning(Date(10,6,2016), ([Subject("Math"), Subject("Anglais")]))
 
-print(papaa)
+print(papa)
 
-something: list[Subject] = papaa.planning.get(str(Date(10,6,2016)))
+something: list[Subject] = papa.planning_list.get(str(Date(10,6,2016)))
 
 print(something[0])
